ee71a56c8e0a34a57a2ec0fab4d8b507732c98f1
Jackson Meade <jacksonmeade@outlook.com>
2025-02-02 16:22:32 -0500

    chore: clear old component files

--- app/dashboard/@modern/catalog/components/ArtistLoadingAvatar.tsx
+++
@@ -1,8 +0,0 @@
-import { Skeleton } from "@mui/joy";
-
-
-export default function ArtistLoadingAvatar() {
-    return (
-        <Skeleton variant="circular" width={"3.2rem"} height={"3.2rem"} />
-    )
-}
