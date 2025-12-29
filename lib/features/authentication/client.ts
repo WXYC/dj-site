a26a6dd5185a8df7b810d87eee7e54b8ac60fbff
Jackson Meade <jacksonmeade@outlook.com>
2025-12-29 16:41:58 -0500

    feat: create auth client

--- lib/features/authentication/client.ts
+++ lib/features/authentication/client.ts
@@ -1,5 +1,12 @@
-import { createAuthClient } from "better-auth/react" // make sure to import from better-auth/react
+import { createAuthClient } from "better-auth/react"
+import { adminClient, usernameClient, jwtClient, organizationClient } from "better-auth/client/plugins"
 
 export const authClient =  createAuthClient({
-    //you can pass client configuration here
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+    plugins: [
+        adminClient(),
+        usernameClient(),
+        jwtClient(),
+        organizationClient(),
+    ]
 });
